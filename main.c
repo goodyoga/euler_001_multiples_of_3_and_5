4fc510d1484fca4f43dbe47598abffd6cd528182
goodyoga <goodyoga.goodyoga@gmail.com>
2013-07-20 01:09:46 +0900

    initial

---
+++ main.c
@@ -0,0 +1,49 @@
+/* Copyright 2013 goodyoga all rights reserved. */
+
+#include <stdio.h>    /* printf */
+#include <stdlib.h>   /* atoi,exit   */
+#include <unistd.h>   /* getopt */
+
+void usage(void);
+
+int main(int argc __attribute__((unused)), char **argv)
+{
+    int i;
+    int sum;
+    int opt, below = 1000;
+    while ( -1 != (opt = getopt(argc, argv, "hb:")) )
+    {
+        if('h' == opt)
+	{
+	  usage();
+	  exit(EXIT_SUCCESS);
+	}
+        else if ('b' == opt)
+	{
+	  below = atoi(optarg);
+	}
+    }
+    for ( i = 1,sum = 0 ; i < below ; i++)
+    {
+        if ( !( i % 3 ) || !( i % 5 ) )
+        {
+            sum += i;
+            printf("%s: add:%d, sum:%d\n", argv[0], i, sum);
+        }
+    }
+    return EXIT_SUCCESS;
+}
+
+
+void usage(void)
+{
+    printf("    http://projecteuler.net/problem=1\n");
+    printf("    [Problem 1] Multiples of 3 and 5\n");
+    printf("    If we list all the natural numbers below 10 \n"
+           "    that are multiples of 3 or 5, we get 3, 5, 6 and 9.\n"
+           "    The sum of these multiples is 23.\n"
+           "    Find the sum of all the multiples of 3 or 5 below 1000.\n\n");
+    printf("    -h: show this help\n"
+           "    -b <number>: solve this between 1 and below <number>\n");
+    return;
+}
